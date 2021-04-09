ed72ac11b8bfc94d2f2caf66e2655a34e199d9a7
mgabriel29 <marvin.gabriel@gmail.com>
2021-04-08 19:47:45 -0700

    content edit

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -8,12 +8,10 @@ function Landing() {
                 <p>I'm a <span>full-stack developer</span> and a <span>graphic designer</span> residing in the Bay Area.</p>
             </div>
             <div className="tagline__2">
-                <p>Clean backends<br/>
-                <span>& </span> 
-                pretty frontends.</p>
+                <p>With a bachelors in graphic design and experience in developing full-stack applications, I create clean and professional web applications. </p>
             </div>
             <div className="button1">
-            <button type="button" class="btn btn-outline-dark" disabled>More to come!</button> 
+            <button type="button" class="btn btn-outline-dark">More to come!</button> 
             </div>
         </div>
     )
